aebe220815ea491e06865ab003c1fb1d0bb3ded4
Andrew Maraev <the_vk@thevk.net>
2026-04-05 17:15:59 -0700

    feat: add basic event loop handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,33 @@
-use anyhow::{Result};
+use anyhow::Result;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
 
+#[derive(Default)]
 pub struct App {
+    window: Option<Window>,
 }
 
 impl App {
 
-    pub fn run(&self) -> Result<()> {
-        Ok(())
+    pub fn run(&mut self) -> Result<()> {
+        let event_loop = EventLoop::new()?;
+
+        event_loop.set_control_flow(ControlFlow::Wait);
+        Ok(event_loop.run_app(self)?)
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(event_loop.create_window(Window::default_attributes()).expect("failed to create a window"));
+    }
 
-    #[test]
-    fn run_returns_success() {
-        let app = App {};
-
-        assert_eq!(app.run().is_ok(), true);
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            _ => (),
+        }
     }
 }
+
